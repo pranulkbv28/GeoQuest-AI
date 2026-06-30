573daa574ea0f91c447c26ab5ac6fbe2433fc75a
Pranaav B V <28bvpranaav@gmail.com>
2026-06-30 18:01:26 +0530

    Add format task to turbo.json with cache disabled

---
+++ apps/api/src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const port: string | number = process.env.PORT ?? 3001;
+
+  await app.listen(port);
+
+  console.log(`🚀 API running on http://localhost:${port}`);
+}
+void bootstrap();
